cc258a042dc097f34f0170b395a4da6e3ffcdaed
rohit <rohitj559@gmail.com>
2019-08-07 07:42:27 -0500

    Creating and working with Tables

--- Useful-SQL-Queries.sql
+++ Useful-SQL-Queries.sql
@@ -1,7 +1,41 @@
-CREATE DATABASE Sample
+-- 2. Creating altering and dropping a database 
 
-ALTER DATABASE Sample MODIFY NAME = SAMPLE1
-EXECUTE sp_renamedb'SAMPLE1', 'SAMPLE2'
+--CREATE DATABASE Sample
 
-DROP DATABASE SAMPLE2
+--ALTER DATABASE Sample MODIFY NAME = SAMPLE1
+--EXECUTE sp_renamedb'SAMPLE1', 'SAMPLE2'
+
+--DROP DATABASE SAMPLE2
+-- ---------------------------------------------------------
+-- 3. Creating and working with Tables
+
+use Sample
+go
+create table tblPerson
+(
+ Id int not null  primary key,
+ Name nvarchar(50) not null,
+ Email nvarchar(50) not null,
+ GenderId int
+)
+
+create table tblGender
+(
+ ID int not null  primary key,
+ Gender nvarchar(10) not null
+)
+
+drop table tblPerson
+
+-- To add a foreign key reference using a query
+Alter table tblPerson 
+add constraint tblPerson_GenderId_FK FOREIGN KEY (GenderId) references tblGender(ID)
+
+-- ---------------------------------------------------------------------------------
+
+
+
+
+
+
 
