92f14c23e95b042fad18c020061d306dbe04ea95
rohit <rohitj559@gmail.com>
2019-08-09 01:07:07 -0500

    Stored procedure output parameters or return values - Part 20

--- Useful-SQL-Queries.sql
+++ Useful-SQL-Queries.sql
@@ -444,7 +444,7 @@ end
 sp_helptext spGetEmployees
 
 -----------------------------------------------------------
-
+-- Stored procedures with output parameters - Part 19
 
 Create Procedure spGetEmployeeCountByGender
 @Gender nvarchar(20),
@@ -475,8 +475,59 @@ sp_help spGetEmployeeCountByGender
 sp_depends spGetEmployeeCountByGender
 
 ------------------------------------------------------------------------------------------
+-- Stored procedure output parameters or return values - Part 20
 
+create Procedure spGetTotalCountOfEmployees1
+@TotalCount int output
+as
+Begin
+ Select @TotalCount = COUNT(ID) from tblEmployee
+End			
 
+Declare @TotalEmployees int
+Execute spGetTotalCountOfEmployees1 @TotalEmployees Output
+Select @TotalEmployees
+
+Create Procedure spGetTotalCountOfEmployees2
+as
+Begin
+ return (Select COUNT(ID) from Employees)
+End
+
+alter Procedure spGetTotalCountOfEmployees2
+as
+Begin
+ return (Select COUNT(ID) from tblEmployee)
+End
+
+Declare @TotalEmployees int
+Execute @TotalEmployees = spGetTotalCountOfEmployees2
+Select @TotalEmployees
+
+Create Procedure spGetNameById1
+@Id int,
+@Name nvarchar(20) Output
+as
+Begin
+ Select @Name = Name from tblEmployee Where Id = @Id
+End
+
+Declare @EmployeeName nvarchar(20)
+Execute spGetNameById1 3, @EmployeeName out
+Print 'Name of the Employee = ' + @EmployeeName
+
+Create Procedure spGetNameById2
+@Id int
+as
+Begin
+ Return (Select Name from tblEmployee Where Id = @Id)
+End
+
+Declare @EmployeeName nvarchar(20)
+Execute @EmployeeName = spGetNameById2 1
+Print 'Name of the Employee = ' + @EmployeeName
+
+------------------------------------------------------------------------------
 
 
 
