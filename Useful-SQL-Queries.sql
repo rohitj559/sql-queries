eb18c8a9d45e2379ca4f4817661af5f29110c1cf
rohit <rohitj559@gmail.com>
2019-08-09 19:34:58 -0500

    DateTime functions in SQL Server - Part 25

--- Useful-SQL-Queries.sql
+++ Useful-SQL-Queries.sql
@@ -625,6 +625,23 @@ From tblPerson
 ------------------------------------------------------------------------------------------
 --DateTime functions in SQL Server - Part 25
 
+CREATE TABLE [tblDateTime]
+(
+ [c_time] [time](7) NULL,
+ [c_date] [date] NULL,
+ [c_smalldatetime] [smalldatetime] NULL,
+ [c_datetime] [datetime] NULL,
+ [c_datetime2] [datetime2](7) NULL,
+ [c_datetimeoffset] [datetimeoffset](7) NULL
+)
+
+select * from [tblDateTime]
+
+INSERT INTO tblDateTime VALUES (GETDATE(),GETDATE(),GETDATE(),GETDATE(),GETDATE(),GETDATE())
+
+---------------------------------------------------------------------------------------------
+--IsDate, Day, Month, Year and DateName DateTime functions in SQL Server - Part 26
+
 
 
 
